fcf94b6f2e8270d97876c2ea1aa2148858b60576
lee5031207 <lee5031207@naver.com>
2020-06-24 21:32:18 +0900

    css수정2

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
                         <span class="icon-bar"></span>
                     </button>
                     <a class="navbar-brand" href="#">
-						PETSTAGRAM
+						Petstagram
                     </a>
                 </div>
                 <div class="collapse navbar-collapse" id="nav_menu">
